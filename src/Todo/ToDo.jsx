8c9499859a59ebb44b1908010a8c61ee2bd99cea
geomontarroyos <gmontarroyosa@gmail.com>
2023-09-01 14:33:34 -0400

    commit

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -2,12 +2,32 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ToDo() {
-   
+    const [lista, setLista] = useState([]);
+    const [atividade, setAtividade] = useState("");
+  
+    const salvar = (e) => {
+      e.preventDefault();
+      setLista([
+        ...lista,
+        {
+          atividade: atividade,
+        },
+      ]);
+      setId(id + 1);
+    };
+  
     return (
-        <div>
-            <h1>Lista de Atividades</h1>
-            <Link to="/">home</Link>
-           
-        </div>
+      <div>
+        <h1>Lista de atividade</h1>
+        <Link to="/"> home </Link>
+        <form onSubmit={salvar}>
+          <input value={atividade}
+            onChange={(e) => setAtividade(e.target.value)}/>
+  
+          <button>ADD</button>
+        </form>
+
+        {lista.map((ativ) => <p>{ativ.atividade}</p>)}
+          </div>
     );
-}
+    }
